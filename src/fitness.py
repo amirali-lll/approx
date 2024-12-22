c65f60fde0bd740b4a6a060c3fa5829831008d0f
amirali-lll <amirali.dst.lll@gmail.com>
2024-12-22 13:12:17 +0330

    incorporate length penalty in fitness calculation

--- src/fitness.py
+++ src/fitness.py
@@ -1,6 +1,10 @@
 import numpy as np
+from src.conf import LENGTH_PENALTY
 
 def fitness(tree, X, Y):
     predictions = np.array([tree.evaluate(x) for x in X])
     mse = np.mean((predictions - Y) ** 2)
+
+    mse += tree.length() / LENGTH_PENALTY
+    
     return float(mse)
