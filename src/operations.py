529bad19cf3b77a5f75ccf5c0efddcb338c709a3
amirali-lll <amirali.dst.lll@gmail.com>
2024-12-22 13:12:39 +0330

    Add reproduction function for crossover of trees in genetic programming

--- src/operations.py
+++ src/operations.py
@@ -2,6 +2,7 @@ import random
 from src.tree import Node
 from src.utils import generate_random_tree
 from src.conf import MUTATION_PROBABILITY
+import copy
 
 
 def mutate(tree, depth=3):
@@ -15,3 +16,23 @@ def mutate(tree, depth=3):
     if tree.right:
         tree.right = mutate(tree.right, depth - 1)
     return tree
+
+
+
+def reproduce(tree1, tree2):
+    """
+    crossover two trees by swapping random subtrees
+    """
+    if tree1 is None:
+        return copy.deepcopy(tree2)
+    if tree2 is None:
+        return copy.deepcopy(tree1)
+    if random.random() < 0.5:
+        value = tree1.value
+        right = copy.deepcopy(tree1.right)
+        left = copy.deepcopy(tree1.left)
+    else:
+        value = tree2.value
+        right = copy.deepcopy(tree2.right)
+        left = copy.deepcopy(tree2.left)
+    return Node(value, left=left, right=right)
