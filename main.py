607cf4f99e142f460d70469dd3244ac696bd99c3
amirali-lll <amirali.dst.lll@gmail.com>
2024-12-25 18:56:24 +0330

    Update target function in main and add accuracy calculation for genetic programming

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ from src.utils import plot_mse_over_generations
 
 def main():
     x_values = np.linspace(-10, 10, 200)
-    y_values = x_values**2 + 3 * x_values + 2
+    y_values = x_values**4 + 5 * x_values + 8
     best_tree, mse_values = genetic_programming(x_values, y_values)
     print(f"Best tree: {best_tree}")
     print(f"Best tree MSE: {mse_values[-1]}")
     print(f"Length of MSE values: {len(mse_values)}")
+    print(f"Accuracy: {accuracy(mse_values[-1], y_values)}")
     plot_mse_over_generations(mse_values)
+
+
+# define the accuracy of the program : 1 - MSE / (variance of true outputs)
+def accuracy(mse, y_values):
+    return 1 - mse / np.var(y_values)
+
 
 if __name__ == "__main__":
     main()
